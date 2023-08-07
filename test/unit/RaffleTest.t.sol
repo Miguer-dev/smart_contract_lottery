3ba6ba96a42471e2f232c382abe77a80ebd14097
Miguel Martinez <miguerdev@gmail.com>
2023-08-07 01:37:56 -0400

    Add new tests

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -5,6 +5,8 @@ import {Test, console} from "forge-std/Test.sol";
 import {Raffle} from "../../src/Raffle.sol";
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract RaffleTest is Test {
     Raffle raffle;
@@ -26,11 +28,26 @@ contract RaffleTest is Test {
 
     event EnteredRaffle(address indexed user);
 
+    modifier raffleEnterAndTimePassed() {
+        vm.prank(TEST_USER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        _;
+    }
+
+    modifier skipFork (){
+        if(block.chainid != 31337 || block.chainid != 5777){
+            return;
+        }
+        _;
+    }
+
     function setUp() external {
         deployRaffle = new DeployRaffle();
         (raffle, helperConfig) = deployRaffle.run();
         vm.deal(TEST_USER, STARTTING_BALANCE);
-        (entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit, linkToken) =
+        (entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit, linkToken,) =
             helperConfig.activeNetworkConfig();
     }
 
@@ -45,7 +62,7 @@ contract RaffleTest is Test {
     function testNotEnoughEntranceFee() public {
         vm.prank(TEST_USER);
         vm.expectRevert(Raffle.Raffle__NotEnoughEntranceFee.selector);
-        raffle.enterRaffle{value: 0.001 ether}();
+        raffle.enterRaffle();
     }
 
     function testRecordUserWhenEnter() public {
@@ -61,11 +78,7 @@ contract RaffleTest is Test {
         raffle.enterRaffle{value: entranceFee}();
     }
 
-    function testCantEnterWhenRaffleIsCalculating() public {
-        vm.prank(TEST_USER);
-        raffle.enterRaffle{value: entranceFee}();
-        vm.warp(block.timestamp + interval * 1);
-        vm.roll(block.number + 1);
+    function testCantEnterWhenRaffleIsCalculating() public raffleEnterAndTimePassed {
         raffle.performUpkeep("");
 
         vm.expectRevert(Raffle.Raffle__NotOpen.selector);
@@ -76,4 +89,107 @@ contract RaffleTest is Test {
     //////////////////////////////
     ///       checkUpkeep      ///
     //////////////////////////////
+
+    function testCheckUpkeepReturnFalseIfHasNoBalance() public {
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        (bool upkeepNeede,) = raffle.checkUpkeep("");
+
+        assert(!upkeepNeede);
+    }
+
+    function testCheckUpkeepReturnFalseIfRaffleNotOpen() public raffleEnterAndTimePassed {
+        raffle.performUpkeep("");
+
+        (bool upkeepNeede,) = raffle.checkUpkeep("");
+
+        assert(!upkeepNeede);
+    }
+
+    function testCheckUpkeepReturnFalseIfEnoughtTimeHasntPassed() public {
+        vm.prank(TEST_USER);
+        raffle.enterRaffle{value: entranceFee}();
+
+        (bool upkeepNeede,) = raffle.checkUpkeep("");
+
+        assert(!upkeepNeede);
+    }
+
+    function testCheckUpkeepReturnTrue() public raffleEnterAndTimePassed {
+        (bool upkeepNeede,) = raffle.checkUpkeep("");
+
+        assert(upkeepNeede);
+    }
+
+    //////////////////////////////
+    ///     performUpkeep      ///
+    //////////////////////////////
+
+    function testPerformUpkeepCanOnlyRunIfCheckUpkeepIsTrue() public raffleEnterAndTimePassed {
+        //expect NotRevert not exist in foundry, only with this is okay, if the function revert the test fail.
+        raffle.performUpkeep("");
+    }
+
+    function testPerformUpkeepRevertIfCheckUpkeepIsFalse() public {
+        uint256 currentBalance = 0;
+        uint256 numUsers = 0;
+        uint256 raffleState = 0;
+
+        vm.expectRevert(
+            abi.encodeWithSelector(Raffle.Raffle__UpkeepNotNeeded.selector, currentBalance, numUsers, raffleState)
+        );
+        raffle.performUpkeep("");
+    }
+
+    function testPerformUpkeepUpdateRaffleStateAndEmitRequestId() public raffleEnterAndTimePassed {
+        //record all event that trigger the next line
+        vm.recordLogs();
+        raffle.performUpkeep("");
+        //Save all logs(events) in a Array type bytes32
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        //You can get the position of the event you need with "forge test --debug <function>", topics[0] = entireEvent and topics[1] = firstParameter
+        bytes32 requestId = entries[1].topics[1];
+
+        Raffle.State rState = raffle.getRaffleState();
+
+        assert(uint256(rState) == 1);
+        assert(uint256(requestId) > 0);
+    }
+
+    //////////////////////////////
+    ///   fulfillRandomWords   ///
+    //////////////////////////////
+
+    //Fuzz Test, Foundry generate randomRequestIds and run the test several times.
+    function testFulfillRandomWordsCanOnlyBeCalledAdterPerformUpkeep(uint256 randomRequestId)
+        public
+        raffleEnterAndTimePassed skipFork
+    {
+        vm.expectRevert("nonexistent request");
+        VRFCoordinatorV2Mock(vrfCoordinator).fulfillRandomWords(randomRequestId, address(raffle));
+    }
+
+    function testFulfillRandomWordsPicksAWinnerResetsAndSendsMoney() public raffleEnterAndTimePassed skipFork{
+        uint256 additionalEntrants = 5;
+        uint256 startingIndex = 1;
+        uint256 previousTimeStamp = raffle.getLastTimestamp();
+        for (uint256 i = startingIndex; i <= additionalEntrants; i++) {
+            address player = address(uint160(i)); //address(1), address(2) ...
+            hoax(player, 1 ether);
+            raffle.enterRaffle{value: entranceFee}();
+        }
+
+        vm.recordLogs();
+        raffle.performUpkeep("");
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        bytes32 requestId = entries[1].topics[1];
+
+        VRFCoordinatorV2Mock(vrfCoordinator).fulfillRandomWords(uint256(requestId), address(raffle));
+
+        assert(uint256(raffle.getRaffleState()) == 0);
+        assert(raffle.getWinner() != address(0));
+        assert(raffle.getUserlenght() == 0);
+        assert(raffle.getLastTimestamp() > previousTimeStamp); 
+    }
 }
